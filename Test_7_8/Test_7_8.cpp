6359f3da7d90dd363506a865aa6cbb000b3a4f53
Karl X <1473931863@qq.com>
2022-03-17 14:06:21 +0800

    Branch and Loop

---
+++ Test_7_8/Test_7_8.cpp
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	//int ch = 0;
+//	//while ((ch = getchar()) != EOF) //EOF-End Of File-文件结束标志
+//	//	putchar(ch);
+//	//return 0;
+//	
+//	int ret = 0;
+//	int ch = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);  //输入密码，并存放在password数据中
+//	//缓冲区剩余\n
+//	while (ch = getchar() != '\n')
+//	{
+//
+//	}
+//	printf("请确认：>");
+//	ret = getchar();  //Y/N
+//	if (ret == 'Y')
+//		printf("确认成功\n");
+//	else
+//		printf("放弃确认\n");
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+
+int main()
+//{
+//	/*int i = 0; //初始化
+//	while (i<10) //判断
+//	{
+//		//.....
+//		i++; //调整
+//	}*/
+//
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{		
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//
+//	int i = 0;
+//	while (i<=10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+int main()
+// {
+//	/*int i = 0;
+//	int j = 0;
+//	for (i=0;i<10;i++)
+//	{
+//		for ( j = 0; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;*/
+//
+//	int x, y;
+//	for (x = 0,y=0;  x< 2 && y<5; ++x,y++)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+{   //循环0次
+	int i = 0;
+	int k = 0;
+	for ( i = 0,k=0 ; k=0 ; i++,k++)
+	{
+		k++;
+	}
+	return 0;
+}
